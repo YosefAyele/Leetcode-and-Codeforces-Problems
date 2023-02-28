7d9c6ec3082f74e2e442748b863a9277ae8681e9
Yosef Ayele <105995267+YosefAyele@users.noreply.github.com>
2023-02-28 10:29:05 +0300

    Time: 35 ms (59.67%), Space: 13.8 MB (93.31%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -1,18 +1,18 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
         
+        
         def recur(num):
             
-            if num < 1:
-                return False
             if num == 1:
                 return True
+            elif num < 1:
+                return False
             
-            if num % 4 != 0:
+            if num%4: 
                 return False
-         
-            if num < 4 and num > 1:
-                return False
+           
+            
             return recur(num//4)
         
         
