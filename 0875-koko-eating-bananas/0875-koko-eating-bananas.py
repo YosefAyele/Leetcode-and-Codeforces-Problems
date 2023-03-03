bfcdcf7a7b8453013f1743d517560dcb66f44c95
Yosef Ayele <105995267+YosefAyele@users.noreply.github.com>
2023-03-03 16:27:08 +0300

    Time: 719 ms (12.54%), Space: 15.4 MB (64.11%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        
+        def totalHours(speed):
+            res = 0
+            for pile in piles:
+                
+                res += max(1,ceil(pile/speed))
+            
+            return res
+        
+        
+        low = 0
+        high = max(piles) + 1
+        
+        while high > low + 1:
+            
+            mid = low + (high - low)//2
+            
+            currTotal = totalHours(mid)
+            # print(speed," ",currTotal)
+            
+            if currTotal > h:
+                low = mid
+                
+            else:
+                # print(high)
+                high = mid
+                
+        return high
+                
+        
+        
+            
