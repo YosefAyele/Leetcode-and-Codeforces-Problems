7402f1c19b418dd27d071a1f987369c5c58cd099
Yosef Ayele <105995267+YosefAyele@users.noreply.github.com>
2023-03-06 17:31:25 +0300

    Time: 152 ms (46.06%), Space: 20.6 MB (22.94%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        
+        n = len(citations)
+        low = -1
+        high = n
+        
+        while high > low + 1:
+            
+            mid = low + (high - low)//2
+            
+            if citations[mid] == n - mid:
+                return citations[mid]
+            elif  citations[mid] < n - mid: 
+                low = mid
+            else:
+                high = mid
+
+        return n - high
