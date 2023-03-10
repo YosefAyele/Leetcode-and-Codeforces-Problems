39835469d6dbcd852527dd3ba877b8947fac9f41
Yosef Ayele <105995267+YosefAyele@users.noreply.github.com>
2023-03-10 17:07:28 +0300

    Time: 43 ms (27.63%), Space: 13.8 MB (63.49%) - LeetHub

--- 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.py
+++ 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.py
@@ -9,20 +9,23 @@ class Solution:
             if idx == len(s):
                 return True
 
-            temp = False
+            
             for end in range(idx+1,len(s)+1):
                 currNum = int(s[idx:end])
                 
                 if last - currNum == 1:
                     
-                    temp = temp or split(end,currNum)
+                    if split(end,currNum):
+                        return True
             
-            return temp
+            return False
         
         ans = False
         for start in range(1, len(s)):
             
             ans =  ans or split(start,int(s[:start]))
+            if ans:
+                return True
         
         
         return ans
