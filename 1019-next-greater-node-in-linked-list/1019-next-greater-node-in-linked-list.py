be277d0f0fc65e8bfdc5d7e538944b2a2e468540
Yosef Ayele <105995267+YosefAyele@users.noreply.github.com>
2023-03-20 17:44:40 +0300

    Time: 336 ms (37.39%), Space: 19.3 MB (7.42%) - LeetHub

---
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def nextLargerNodes(self, head: Optional[ListNode]) -> List[int]:
+        
+        stack = []
+        listVersion = []
+        curr = defaultdict(int)
+        temp = head
+        idx = 0
+        while temp:
+            
+            while stack and temp.val > stack[-1][0]:
+                curr[stack.pop()[1]] = temp.val
+            stack.append((temp.val,idx))
+            
+            listVersion.append(temp.val)
+            idx += 1
+            temp = temp.next
+        
+        return [curr[i] for i in range(len(listVersion))]
