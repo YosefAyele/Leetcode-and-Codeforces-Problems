cabeb1d52a8d780ab17309912e3f2508c1ed62ba
Yosef Ayele <105995267+YosefAyele@users.noreply.github.com>
2023-04-27 13:03:28 +0300

    Time: 857 ms (5.02%), Space: 59.5 MB (5.74%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -6,19 +6,22 @@ class Solution:
         res = []
         for position in n_permutations:
             
-            checkd1 = set()
-            checkd2 = set()
+            checked1 = defaultdict(int)
+            checked2 = defaultdict(int)
             
             for i,col in enumerate(position):
-                checkd1.add(i-col)
-                checkd2.add(i+col)
-            if len(checkd1) == len(checkd2) == n:
+                checked1[i - col] += 1
+                checked2[i + col] += 1
+                
+                if checked1[i-col] > 1 or checked2[i + col] > 1:
+                    break
+            else:
                 curr =  [['.']*n for _ in range(n)]
                 for row,col in enumerate(position):
                     curr[row][col] = "Q"
                     curr[row] = "".join(curr[row])
                 res.append(curr)
-        
+
         return res
                 
                 
