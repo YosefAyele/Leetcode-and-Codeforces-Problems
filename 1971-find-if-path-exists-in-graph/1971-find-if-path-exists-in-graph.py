17239b2f7a9a16fff163ac9bbce753af66632af6
Yosef Ayele <105995267+YosefAyele@users.noreply.github.com>
2023-05-17 12:55:23 +0300

    Time: 2003 ms (55.46%), Space: 107.1 MB (60.31%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,23 +1,37 @@
+# UnionFind class
+class UnionFind:
+    def __init__(self, size):
+        self.rep = {i:i for i in range(size)}
+        self.rank = [1]*size
+    def representative(self, x):
+        
+        x = self.rep[x]
+        
+        while x != self.rep[x]:
+            x = self.rep[x]
+        
+        return x
+		
+    def union(self, x, y):
+        xrep = self.representative(x)
+        yrep = self.representative(y)
+        
+        if self.rank[xrep] > self.rank[yrep]:
+            self.rep[yrep] = xrep
+            self.rank[xrep] += self.rank[yrep]
+        else:
+            self.rep[xrep] = yrep
+            self.rank[yrep] += self.rank[xrep]     
+
+    def connected(self, x, y):
+        return self.representative(x) == self.representative(y)
+
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        graph = defaultdict(list)
         
-        for start, end in edges:
-            graph[start].append(end)
-            graph[end].append(start)
-            
-        visited = set()
+        uf = UnionFind(n)
         
-        def dfs(node):
-            
-            if node == destination:
-                return True
-            visited.add(node)
-            
-            for child in graph[node]:
-                if child not in visited:
-                    if dfs(child):
-                        return True
-                        
-        return dfs(source)
-                
+        for x,y in edges:
+            uf.union(x,y)
+        return uf.connected(source,destination)
+    
