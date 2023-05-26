f647103ba87b6fae9c862daf52f36ff7d46574c7
Yosef Ayele <105995267+YosefAyele@users.noreply.github.com>
2023-05-26 17:49:27 +0300

    Time: 771 ms (14.55%), Space: 16.2 MB (24.39%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        
+        memo = defaultdict(int)
+        memo[0] = 0
+        memo[1] = 1
+        
+        def fibo(n):
+            if n <= 1:
+                return n
+            memo[n] = fibo(n-1) + fibo(n-2)
+            return memo[n]
+        return fibo(n)
+    
