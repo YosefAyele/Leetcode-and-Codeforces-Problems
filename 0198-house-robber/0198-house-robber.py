dce64f4ffa1dcadd160aed85235979ff592afee7
Yosef Ayele <105995267+YosefAyele@users.noreply.github.com>
2023-05-26 19:25:11 +0300

    Time: 54 ms (8.20%), Space: 16.4 MB (13.07%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = [-1]*len(nums)
+        
+        def dp(house):
+            if house >= len(nums):
+                return 0
+            if memo[house] == -1:
+                memo[house] = max(nums[house] + dp(house+2),dp(house+1))
+            return memo[house]
+        
+        dp(0)
+        return max(memo)
