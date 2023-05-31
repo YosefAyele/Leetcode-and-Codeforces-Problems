91b99a6dbfc61871873b8e0280b14fe81d821bfa
Yosef Ayele <105995267+YosefAyele@users.noreply.github.com>
2023-05-31 10:19:25 +0300

    Time: 512 ms (98.47%), Space: 17.3 MB (30.20%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -4,12 +4,12 @@ class Solution:
         queue = deque()
         queue.append((0,0))
      
-        ans = inf
+        
         
         while queue:
             total, step = queue.popleft()
             if total == amount:
-                ans = min(ans,step)
+                return step
             elif total < amount:
                 for coin in coins:
                     newtotal = total + coin
@@ -17,6 +17,6 @@ class Solution:
                         queue.append((newtotal,step + 1))
                         seen.add(newtotal)
                         
-        return ans if ans < inf else -1
+        return -1
                         
                         
